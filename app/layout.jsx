2fad9e6894f58ddbaaf6ddb087aa4689f746e864
ProgrammerIosif <programmeriosif@protonmail.com>
2023-10-02 20:50:41 +0300

    added vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Navbar from './Navbar'
+import { Analytics } from '@vercel/analytics/react'
 
 import { Poppins } from 'next/font/google'
 const font = Poppins({ subsets: ['latin'], weight: ['400','500','600'] })
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
           {children}
           <Navbar/>
         </div>
+        <Analytics/>
       </body>
     </html>
   )
